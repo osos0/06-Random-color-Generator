f2418586e5ef5c2e8bc2b914c2cf7d6deb86c3c1
unknown <osama_magdy0@yahoo.com>
2022-11-15 06:10:53 +0200

    final javescript file from my home

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const divEl = document.querySelector(".conaner");
 // console.log(divEl.innerHTML);
 
-for (let i = 0; i < 30; i++) {
-  const creatEl = document.createElement("div");
-  creatEl.classList.add("colorDef");
-  divEl.appendChild(creatEl);
+function ranColor() {
+  for (let i = 0; i < 30; i++) {
+    const creatEl = document.createElement("div");
+    creatEl.classList.add("colorDef");
+    // creatEl.classList.add("colorDef");  //another answer
+    const inOfColor = "0123456789abcdef";
+    let num1 = Math.floor(Math.random() * inOfColor.length);
+    let num2 = Math.floor(Math.random() * inOfColor.length);
+    let num3 = Math.floor(Math.random() * inOfColor.length);
+    let num4 = Math.floor(Math.random() * inOfColor.length);
+    let num5 = Math.floor(Math.random() * inOfColor.length);
+    let num6 = Math.floor(Math.random() * inOfColor.length);
+    let val = `#${inOfColor[num1]}${inOfColor[num2]}${inOfColor[num3]}${inOfColor[num4]}${inOfColor[num5]}${inOfColor[num6]}`;
+    creatEl.style.backgroundColor = `${val}`;
+    creatEl.innerHTML = `${val}`;
+    divEl.appendChild(creatEl);
+  }
 }
+ranColor();
